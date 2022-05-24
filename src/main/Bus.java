e1d1db2518eff2c666f8c373dcc6d3230d0126e2
unknown <padinmichael201@gmail.com>
2022-05-24 19:43:19 +0800

    initial commit

---
+++ src/main/Bus.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Bus {
+   private int busNo;
+   private String busName;
+   private String destination;
+   private String origin;
+   private int capacity;
+
+
+    Bus(int busNo, String busName, String destination, String origin, int capacity) {
+        this.busNo = busNo;
+        this.busName = busName;
+        this.destination = destination;
+        this.origin = origin;
+        this.capacity = capacity;
+    }
+}
